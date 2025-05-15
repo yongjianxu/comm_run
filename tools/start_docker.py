10151c9fc146cbf3d1a2ca2fc2cd62d47616fd3e
Yongjian <35827554@qq.com>
2025-05-15 20:16:50 +0800

    Update start_docker.py

--- tools/start_docker.py
+++ tools/start_docker.py
@@ -7,6 +7,7 @@ class mycontainer:
         u = subprocess.run(["id", "-u"], capture_output=True, text=True).stdout.replace("\n","")
         g = subprocess.run(["id", "-g"], capture_output=True, text=True).stdout.replace("\n","")
         cmd = "docker run --name mytest --cap-add=SYS_NICE --cap-add=IPC_LOCK --cap-add=SYS_ADMIN --gpus all --ulimit memlock=-1 --ulimit stack=2000000000 --init -ti -p 12345:12345 -p 2379:2379 --shm-size 32g --network=host -v /usr/src:/usr/src -v /lib/modules:/lib/modules --device=/dev/infiniband/ " + " " + image + " " + "bash"
+        #cmd = "docker run --name mytest --cap-add=SYS_NICE --cap-add=IPC_LOCK --cap-add=SYS_ADMIN --gpus all --ulimit memlock=-1 --ulimit stack=2000000000 --init -ti -p 12345:12345 -p 2379:2379 --shm-size 32g --network=host -v /usr/src:/usr/src -v /lib/modules:/lib/modules --device=/dev/infiniband/ --device=/dev/gdrdrv " + " " + image + " " + "bash"
         print(cmd) 
         os.system(cmd) 
 
